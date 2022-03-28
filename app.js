320e38f27a60b3873a48456bd1dac58a1f0302bf
djlahre97 <dhananjaylahre@gmail.com>
2022-03-28 16:54:54 +0530

    add shiksha to app.js

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const fs = require("fs");
 const path = require("path");
 
 const collegeSearch = require("./src/college_search");
+const shiksha = require("./src/shiksha");
 
 (async () => {
+  console.log("Scraper running...");
   const data1 = await collegeSearch();
+  const data2 = await shiksha();
   const path1 = path.join(__dirname, "/data/college-search.json");
+  const path2 = path.join(__dirname, "/data/shiksha.json");
   fs.writeFileSync(path1, JSON.stringify(data1));
+  fs.writeFileSync(path2, JSON.stringify(data2));
+  console.log("Scraping complete");
 })();
